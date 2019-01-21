48ccba33b5c20fcd6968efbca3a85a95a8f2ecc7
Luiz Flávio <l.flaviod@yahoo.com.br>
2019-01-21 13:29:49 -0200

    Projeto mostrando a mensagem HELLO RUINHA'S GUYS

--- Git Teste 02/Program.cs
+++ Git Teste 02/Program.cs
@@ -3,7 +3,7 @@
 namespace Git_Teste_02 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ruinha's guys!");
         }
     }
 }
